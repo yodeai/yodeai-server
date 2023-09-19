ea4e9699ed4f267c6d687b51907a64bbf0a2058f
niloufars <niloufar.salehi@gmail.com>
2023-09-19 11:18:16 -0700

    using huggingface embeddings.

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import os
 from fastapi import FastAPI
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 
 app = FastAPI()
 
-
 @app.get("/")
 def demo():
-    return {"message": "Hello World"}
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+    hf_embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+
+    # Compute embeddings for a list of sentences
+    sentences = ["This is a sentence.", "This is another sentence."]
+    embeddings = hf_embeddings.embed_documents(sentences)
+
+    # Store the length of embeddings
+    lengths = []
+    for i, embedding in enumerate(embeddings):
+        print(f"Sentence {i+1} embedding: {embedding}")
+        lengths.append(len(embedding))
+
+    return {"embedding_lengths": lengths}
 
 if __name__ == "__main__":
     import uvicorn

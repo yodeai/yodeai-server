1045bd74d6cf14b5e96377d6cc6aa01fded88fed
niloufars <niloufar.salehi@gmail.com>
2023-09-23 16:35:43 -0700

    fixed function name error.

--- answerQuestionLens.py
+++ answerQuestionLens.py
@@ -1,4 +1,4 @@
 
-def answer_question(question: str, lensID: str) -> str:
+def answer_question_lens(question: str, lensID: str) -> str:
     response = "This is a test response from the backend, and the question is: " + question + " and the lensID is: " + lensID
     return response

5e4e5d90f99914c5b73250c6b9918d2c0da49cd4
Afshi7n <142748141+Afshi7n@users.noreply.github.com>
2023-09-24 20:04:57 -0700

    working on answerQuestionLens

--- answerQuestionLens.py
+++ answerQuestionLens.py
@@ -1,3 +1,7 @@
+def answer_question_lens(question: str, lensID: str) -> str:
+    response = "This is a test response from the backend, and the question is: " + question + " and the lensID is: " + lensID
+    return response
+
 def test_answer_question_lens():
     question = "What is the meaning of life?"
     lensID = "2"
@@ -6,11 +10,6 @@ def test_answer_question_lens():
     response = answer_question_lens(question, lensID)
     print(response)
     
-
-def answer_question_lens(question: str, lensID: str) -> str:
-    response = "This is a test response from the backend, and the question is: " + question + " and the lensID is: " + lensID
-    return response
-
 
 if __name__ == "__main__":
     test_answer_question_lens()

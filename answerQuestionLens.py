8288d1d19fc3a8d1b6d9013ae0abe951087f6e92
Afshi7n <142748141+Afshi7n@users.noreply.github.com>
2023-10-07 14:50:37 -0700

    cleaned up some comments

--- answerQuestionLens.py
+++ answerQuestionLens.py
@@ -64,10 +64,10 @@ def answer_question_lens(question: str, lensID: str, userID: str):
     for d in relevant_chunks:        
         text += d['content'] + "\n\n"        
     prompt = "You are answering questions asked by a user. Answer the question: " + question + " in a helpful and concise way and in at most one paragraph, using the following text inside tripple quotes:\n '''" + text + "''' \n <<<REMEMBER:  If the question is irrelevant to the text, do not try to make up an answer, just say that the question is irrelevant to the context.>>>"
-    sys.stdout.write("\n\n\n----------prompt:\n")
-    sys.stdout.write(prompt)
-    clearConsole(prompt)
-    print("starting to get completion")
+    #sys.stdout.write("\n\n\n----------prompt:\n")
+    #sys.stdout.write(prompt)
+    
+    
     # Record the start time for get_completion
     get_completion_start_time = time.time()
     response = get_completion(prompt)
@@ -78,8 +78,8 @@ def answer_question_lens(question: str, lensID: str, userID: str):
     #if getRel == None or getRel <= relevanceThreshold:
     #    response = notFound
     metadata = {"blocks": list(set(relevant_block_ids))}
+    
     print("done with process vector search")
-    # Print the total time taken
     print(f"Total time taken: {time.time() - start_time:.2f} seconds")
 
     return {

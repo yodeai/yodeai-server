f05d2349652ec99bfdee77e6a887a7afa21069fa
Dana Feng <danafeng@berkeley.edu>
2023-10-16 14:19:09 -0700

    change serialization

--- config/celery_utils.py
+++ config/celery_utils.py
@@ -8,14 +8,13 @@ def create_celery():
     celery_app = current_celery_app
     celery_app.config_from_object(settings, namespace='CELERY')
     celery_app.conf.update(task_track_started=True)
-    celery_app.conf.update(task_serializer='pickle')
-    celery_app.conf.update(result_serializer='pickle')
-    celery_app.conf.update(accept_content=['pickle', 'json'])
+    celery_app.conf.update(task_serializer='json')
+    celery_app.conf.update(result_serializer='json')
+    celery_app.conf.update(accept_content=['json'])
     celery_app.conf.update(result_expires=200)
     celery_app.conf.update(result_persistent=True)
     celery_app.conf.update(worker_send_task_events=False)
     celery_app.conf.update(worker_prefetch_multiplier=1)
-
     return celery_app
 
 
